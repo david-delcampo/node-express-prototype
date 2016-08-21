eefca09a562522050e7f26c31afafeb486b5742d
David <david@cincoinfo.com>
2016-08-21 15:58:25 +0200

    bodyParser para el tratamiento de los datos en el cuerpo del POST
    Teniamos la dependencia pero no haciamos uso de ella, bodyParser. Lo
    dejamos configurado para el uso de los datos en el cuerpo POST.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // statis: js,img,css
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
